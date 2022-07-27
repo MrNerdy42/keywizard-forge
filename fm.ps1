0cfc195d00cca60c13a2c4cc0a9e8f389544d082
Joseph Redmond <jdredmond2000@gmail.com>
2022-07-27 13:51:08 -0400

    Moved stage to proper place.

--- fm.ps1
+++ fm.ps1
@@ -7,8 +7,6 @@ git fetch fabric-origin master
 If ($LASTEXITCODE -ne 0) {Exit}
 git checkout FETCH_HEAD src
 If ($LASTEXITCODE -ne 0) {Exit}
-git add .
-If ($LASTEXITCODE -ne 0) {Exit}
 
 If (Test-Path '.\src\main\java\mrnerdy42\keywizard\KeyWizard.java') {
     Remove-Item -Path '.\src\main\java\mrnerdy42\keywizard\KeyWizard.java' -Force
@@ -21,4 +19,8 @@ ForEach ($f in $srcFiles) {
     $content = $content -replace 'mrnerdy42.keywizard.KeyWizard', 'mrnerdy42.keywizard.forge.KeyWizard'
     $content | Set-Content -Path $f.PSPath
 }
+
+git add .
+If ($LASTEXITCODE -ne 0) {Exit}
+
 Write-Host 'Basic merge complete. Remeber to manually check for errors before committing.'
